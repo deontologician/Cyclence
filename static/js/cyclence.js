cb7de45ef49c1e00f5f23a49a97c6517efe323d6
Josh Kuhn <deontologician@gmail.com>
2012-10-30 21:55:42 -0400

    Task list now refreshes after a completion, causing the list to always be up to date

--- static/js/cyclence.js
+++ static/js/cyclence.js
@@ -9,26 +9,26 @@ function today(){
     return '' + d.getFullYear() + '-' + (d.getMonth() + 1) + '-' + d.getDate();
 }
 
-
 function TaskCtrl($scope, $resource) {
     
-    $scope.Tasks = $resource('/api/tasks');
+    var Tasks = $resource('/api/tasks');
     $scope.Completion = $resource('/api/tasks/:task_id/completions/:completed_on',
                             {task_id: '@task_id', completed_on: '@completed_on'},
                                   {complete : 
                                    {method: 'PUT'}
                                   });
+    $scope.today = new Date();
 
-    
-    $scope.tasks = $scope.Tasks.query(function () {
+    var on_query = function () {
         $scope.current_task = null;
         $scope.complete = function(task) {
             $scope.current_task = task;
-            $scope.gravatar_border = task.hsl;
             $scope.Completion.complete({task_id: task.id,
                                         completed_on: today()});
+            $scope.tasks = Tasks.query(on_query);
         }
-    });
+    }
+    $scope.tasks = Tasks.query(on_query);
 }
 
 
