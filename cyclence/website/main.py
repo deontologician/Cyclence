28f7b5086cff77a5002f3181dbfc1a147873aa73
Josh Kuhn <deontologician@gmail.com>
2012-12-26 00:00:14 -0500

    Tabs are now individual pages, handlers and their urls are now unified

--- cyclence/website/main.py
+++ cyclence/website/main.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 
 import os
 import os.path
+from os.path import join as ojoin
 from base64 import urlsafe_b64decode as b64decode
 from uuid import uuid4
 from datetime import date, datetime
@@ -11,13 +12,13 @@ from tornado.httpclient import HTTPError
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from cyclence.Calendaring import User, Task, Completion, Notification
+import cyclence.Calendaring as orm
 from cyclence.utils import date_str
 
-uuidre = r'[\dA-Fa-f]{8}-[\dA-Fa-f]{4}-[\dA-Fa-f]{4}'\
+UUID_REGEX = r'[\dA-Fa-f]{8}-[\dA-Fa-f]{4}-[\dA-Fa-f]{4}'\
           '-[\dA-Fa-f]{4}-[\dA-Fa-f]{12}'
 
-datere = r'[\d]{4}-[\d]{2}-[\d]{2}'
+DATE_REGEX = r'[\d]{4}-[\d]{2}-[\d]{2}'
 
 def parsedate(datestr):
     'Parses a date in ISO8601 format'
@@ -42,29 +43,33 @@ class BaseHandler(web.RequestHandler):
             email = self.get_secure_cookie('user')
             if not email:
                 return None
-            self._user = self.session.query(User).filter(User.email == email).first()
+            self._user = self.session.query(orm.User).filter(orm.User.email == email).first()
         return self._user
+
+def build_handlers(*args):
+    return [(h.url, h) for h in args]
 
 class CyclenceApp(web.Application):
     r'''Customized application for Cyclence that includes database
     initialization'''
     def __init__(self, debug=False):
-        handlers = [(r"/", MainHandler),
-                    (r"/auth/google", GoogleHandler),
-                    (r"/logout", LogoutHandler),
-                    (r"/tasks", TasksHandler),
-                    (r"/tasks/({})".format(uuidre), TaskHandler),
-                    (r"/tasks/({})/share".format(uuidre), TaskShareHandler),
-                    (r"/tasks/({})/completions".format(uuidre),
-                     CompletionsHandler),
-                    (r"/tasks/({})/completions/({})".format(uuidre, datere),
-                     CompletionHandler),
-                    (r"/notifications/({})".format(uuidre), NotificationHandler),
-                    (r"/invite", InviteHandler),
-                    ]
+        handlers = build_handlers(Main,
+                                  Login,
+                                  Logout,
+                                  Google,
+                                  Tasks,
+                                  Task,
+                                  NewTask,
+                                  TaskShare,
+                                  Completion,
+                                  Notifications,
+                                  Notification,
+                                  Friends,
+                                  Invite,
+                                  )
         settings = dict(
             cookie_secret=os.getenv('CYCLENCE_COOKIE_SECRET'),
-            login_url='/',
+            login_url='/login',
             template_path='tpl',
             debug=True if os.getenv('CYCLENCE_DEBUG') == 'true' else False,
             static_path=os.path.join(os.path.dirname(__file__), "../../static"),
@@ -73,94 +78,31 @@ class CyclenceApp(web.Application):
         connstr = os.getenv('CYCLENCE_DB_CONNECTION_STRING')
         self.session = sessionmaker(bind=create_engine(connstr, echo=debug))()
 
-class TasksHandler(BaseHandler):
-    '''Allows creation of tasks'''
+class Main(BaseHandler):
+    url = "/"
 
     @web.authenticated
-    def post(self):
-        '''Adds a task to the current user'''
-        t = Task(self.get_argument('taskname'),
-                 int(self.get_argument('length')),
-                 self.get_argument('firstdue'),
-                 self.get_argument('allowearly', 'off') == 'on',
-                 int(self.get_argument('points', 100)),
-                 int(self.get_argument('decay_length', self.get_argument('length'))),
-                 set(self.get_argument('tags', '').replace(',',' ').split()),
-                 self.get_argument('notes', None))
-        t.user_email = self.current_user.email
-        self.current_user.tasks.append(t)
-        self.session.commit()
-        self.redirect('/')
-
-class TaskHandler(BaseHandler):
-    r'''Handles updates to a task'''
-    pass
-
-class TaskShareHandler(BaseHandler):
-    r'''Handles sharing tasks'''
-
-    @web.authenticated
-    def post(self, task_id):
-        try:
-            task = self.session.query(Task).filter_by(task_id=task_id).one()
-            if task not in self.current_user.tasks:
-                raise Exception('User does not own this task')
-            email = self.get_argument('friend', None)
-            if email is None:
-                raise Exception('Email argument not given')
-            friend = self.session.query(User).filter_by(email=email).one()
-            if friend not in self.current_user.friends:
-                raise Exception('Cannot share a task with someone who is not a friend.')
-            friend.share_task(task=task, sharer=self.current_user)
-            self.session.commit()
-        except Exception as e:
-            print(str(e))
-        finally:
-            self.redirect('/')
-
-class CompletionsHandler(BaseHandler):
-    @web.authenticated
-    def get(self, task_id):
-        task = filter(lambda t: t.task_id == task_id, self.current_user.tasks)
-        if len(task) == 0:
-            raise HTTPError(404, 'Non existant task or not authorized')
-        self.write(escape.json_encode([
-                    {'completed_on': c.completed_on.isoformat(),
-                     'task_id': str(c.task_id),
-                     'points_earned': c.points_earned,
-                     'recorded_on': c.recorded_on.isoformat(),
-                     'days_late': c.days_late} for c in task[0].completions]))
-
-
-class CompletionHandler(BaseHandler):
-    @web.authenticated
-    def post(self, task_id, completed_on):
-        completion_date = parsedate(completed_on)
-        task = self.session.query(Task).filter(Task.task_id == task_id).one()
-        if task.last_completed is None or completion_date > task.last_completed:
-            task.complete(self.current_user, parsedate(completed_on))
-            self.session.commit()
-        else:
-            self.current_user.notify('error', 
-                                     "You already completed '{}' on {}"
-                                     .format(task.name, 
-                                             date_str(completion_date)),
-                                     task.task_id)
-            self.session.commit()
-        self.redirect('/')
-
-class MainHandler(BaseHandler):
     def get(self):
-        if not self.current_user:
-            self.render('logged_out.html')
-        else:
-            self.session.refresh(self.current_user)
-            self.render('main_page.html',
-                        user=self.current_user,
-                        today=date.today(),
-                        tab=self.get_argument('tab', None))
+        self.session.refresh(self.current_user)
+        self.render('tasklist.html')
 
-class GoogleHandler(BaseHandler, auth.GoogleMixin):
+class Login(BaseHandler):
+    url = ojoin(Main.url, "login")
+
+    def get(self):
+        self.render('login.html')
+     
+class Logout(BaseHandler):
+    url = ojoin(Main.url, 'logout')
+
+    @web.authenticated
+    def get(self):
+        self.clear_cookie('user')
+        self.redirect(Login.url)
+
+class Google(BaseHandler, auth.GoogleMixin):
+    url = ojoin(Main.url, "auth", "google")
+
     @web.asynchronous
     def get(self):
         if self.get_argument("openid.mode", None):
@@ -172,40 +114,131 @@ class GoogleHandler(BaseHandler, auth.GoogleMixin):
         if not user:
             raise web.HTTPError(500, "Google auth failed")
         self.set_secure_cookie('user', user['email'])
-        if not self.session.query(User).filter_by(email=user['email']).first():
-            usr = User(email=user['email'],
+        if not self.session.query(orm.User).filter_by(email=user['email']).first():
+            usr = orm.User(email=user['email'],
                        name=user.get('name'),
                        firstname=user.get('first_name'),
                        lastname=user.get('last_name'))
             self.session.add(usr)
             self.session.commit()
-        self.redirect('/')
-        
-class LogoutHandler(BaseHandler):
+        self.redirect(Main.url)
+
+class Tasks(BaseHandler):
+    '''Allows creation of tasks'''
+    
+    url = ojoin(Main.url, "tasks")
+
     @web.authenticated
     def get(self):
-        self.clear_cookie('user')
-        self.redirect('/')
+        '''Renders the task list'''
+        self.render('tasklist.html')
 
-class NotificationHandler(BaseHandler):
+    @web.authenticated
+    def post(self):
+        '''Adds a task to the current user'''
+        t = orm.Task(self.get_argument('taskname'),
+                 int(self.get_argument('length')),
+                 self.get_argument('firstdue'),
+                 self.get_argument('allowearly', 'off') == 'on',
+                 int(self.get_argument('points', 100)),
+                 int(self.get_argument('decay_length', self.get_argument('length'))),
+                 set(self.get_argument('tags', '').replace(',',' ').split()),
+                 self.get_argument('notes', None))
+        t.user_email = self.current_user.email
+        self.current_user.tasks.append(t)
+        self.session.commit()
+        self.redirect(Tasks.url)
+
+class Task(BaseHandler):
+    r'''Handles updates to a task'''
+
+    url = ojoin(Main.url, "tasks", "({})".format(UUID_REGEX))
+
+class NewTask(BaseHandler):
+    r'''Allows creating a new task'''
+    
+    url = ojoin(Main.url, 'newtask')
+    
+    def get(self):
+        self.render('newtask.html')
+    
+
+class TaskShare(BaseHandler):
+    r'''Handles sharing tasks'''
+    
+    url = ojoin(Task.url, "share")
+
+    @web.authenticated
+    def post(self, task_id):
+        try:
+            task = self.session.query(orm.Task).filter_by(task_id=task_id).one()
+            if task not in self.current_user.tasks:
+                raise Exception('User does not own this task')
+            email = self.get_argument('friend', None)
+            if email is None:
+                raise Exception('Email argument not given')
+            friend = self.session.query(orm.User).filter_by(email=email).one()
+            if friend not in self.current_user.friends:
+                raise Exception('Cannot share a task with someone who is not a friend.')
+            friend.share_task(task=task, sharer=self.current_user)
+            self.session.commit()
+        except Exception as e:
+            print(str(e))
+        finally:
+            self.redirect('/')
+
+class Completion(BaseHandler):
+    
+    url = ojoin(Task.url, "completions", "({})".format(DATE_REGEX))
+
+    @web.authenticated
+    def post(self, task_id, completed_on):
+        completion_date = parsedate(completed_on)
+        task = self.session.query(orm.Task).filter(orm.Task.task_id == task_id).one()
+        if task.last_completed is None or completion_date > task.last_completed:
+            task.complete(self.current_user, parsedate(completed_on))
+            self.session.commit()
+        else:
+            self.current_user.notify('error', 
+                                     "You already completed '{}' on {}"
+                                     .format(task.name, 
+                                             date_str(completion_date)),
+                                     task.task_id)
+            self.session.commit()
+        self.redirect(Main.url)
+
+
+class Notifications(BaseHandler):
+    '''Displays all notifications for a user'''
+
+    url = ojoin(Main.url, 'notifications')
+
+    @web.authenticated
+    def get(self):
+        self.render('notifications.html')
+
+class Notification(BaseHandler):
+    
+    url = ojoin(Notifications.url, "({})".format(UUID_REGEX))
+    
     @web.authenticated
     def post(self, notification_id): 
-        note = self.session.query(Notification).filter_by(notification_id=notification_id).one()
+        note = self.session.query(orm.Notification).filter_by(notification_id=notification_id).one()
         if note not in self.current_user.notifications:
             pass # will just redirect
         elif self.get_argument('delete', None) == 'true':
             self.current_user.notifications.remove(note)
             self.session.commit()
         elif note.noti_type == 'befriend' and self.get_argument('accept', 'false') == 'true':
-            friend = self.session.query(User).filter_by(email=note.sender).one()
+            friend = self.session.query(orm.User).filter_by(email=note.sender).one()
             self.current_user._followers.append(friend)
             friend.notify('message', '{.name} has accepted your friend request'
                           .format(self.current_user))
             self.current_user.notifications.remove(note)
             self.session.commit()
         elif note.noti_type == 'share' and self.get_argument('accept', 'false') == 'true':
-            task = self.session.query(Task).filter_by(task_id=note.task_id).one()
-            sender = self.session.query(User).filter_by(email=note.sender).one()
+            task = self.session.query(orm.Task).filter_by(task_id=note.task_id).one()
+            sender = self.session.query(orm.User).filter_by(email=note.sender).one()
             self.current_user.tasks.append(task)
             self.current_user.notify('message', "You have accepted the task '{.name}'".
                                      format(task))
@@ -213,16 +246,27 @@ class NotificationHandler(BaseHandler):
                           .format(self.current_user, task))
             self.current_user.notifications.remove(note)
             self.session.commit()
-        self.redirect('/?tab=notifications')
+        self.redirect(Notifications.url)
 
-class InviteHandler(BaseHandler):
+class Friends(BaseHandler):
+    '''Handles the list of friends'''
+    url = ojoin(Main.url, "friends")
+    
+    @web.authenticated
+    def get(self):
+        self.render('friendlist.html')
+
+class Invite(BaseHandler):
+    
+    url = ojoin(Main.url, "invite")
+    
     @web.authenticated
     def post(self):
         email = self.get_argument('email')
         if email is None:
-            redirect('/')
+            redirect(FriendList.url)
             return
-        potential_friend = self.session.query(User).filter_by(email=email).one()
+        potential_friend = self.session.query(orm.User).filter_by(email=email).one()
         if potential_friend == self.current_user:
             self.current_user.notify('error',
                                      'Forever Alone: you tried to befriend yourself')
